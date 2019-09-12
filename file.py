8d13c1dbb58df6867dcc9fb6d9b025f74b02dcad
tito18 <tupapaenpanty@gmail.com>
2019-09-12 04:33:15 -0500

    Solo falta crear el archivo json

--- file.py
+++ file.py
@@ -4,7 +4,8 @@ import html5lib
 import base64
 from PIL import Image
 import io
-
+import string
+import json
 
 try:
     html = urlopen("https://ofertadebienes.com//o_list.asp?g=1&op=0&p=0").read().decode()
@@ -17,17 +18,46 @@ except URLError:
     print("Server down or incorrect domain")
 
 else:
-  
+
+    data = {} 
+    data['property'] = []
+    data['property'].append({
+        'description': ' ',
+        'image': ' ',
+        'price': ' '
+    })
+       
     res = BeautifulSoup(html, "html5lib")
     
     itemTags = res.findAll("div" , class_="item")
     
     for tag in itemTags:
        
+       #Imagen
        imagen = tag.find("img", class_="img-thumbnail")
+       imagenString = str(imagen)
+       ima = imagenString.rsplit("src=""")
+       ima2 = ima[1].split('/>')
+       imagenURL = ima2[0].split('"')
+       
+       #Descripcion       
        description = tag.find("small")
+
+       #Precio
        price = tag.find("h4")
-       print(imagen)    #si quieres imprimir el precio por price.getText() dentro del print
+
+       #JSON
+       data['property'].append({
+           'description': description.getText(),
+           'image': imagenURL[1],
+           'price': price.getText()
+       })
+           
+       #print(imagenURL[0])    #si quieres imprimir el precio por price.getText() dentro del print
+
+print(data['property'])
+#with open('data.json', 'w') as json_file:
+    #json.dump(data, json_file)
     
     
 
