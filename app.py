8f512dc3373c8295521ccc96bbc1766cd09add52
tito18 <enocastcorreodetrabajo@gmail.com>
2019-09-12 22:04:21 -0500

    Ahora scrapea todo los otros datos de las otras direcciones

--- app.py
+++ app.py
@@ -19,12 +19,13 @@ else:
 
     data = {} 
     data['property'] = []
+    data['item'] = []      #guardara los datos de las imagenes y mas descripcion cuando entras al link
            
     res = BeautifulSoup(html, "html5lib")
     
-    itemTags = res.findAll("div" , class_="item")
+    itemsTags = res.findAll("div" , class_="item")
     
-    for tag in itemTags:
+    for tag in itemsTags:
        
        #Imagen
        imagen = tag.find("img", class_="img-thumbnail")
@@ -42,21 +43,73 @@ else:
        #Link
        linkRAW = tag.find("a", href_="")
        linkString = str(linkRAW)
-       linkString1 = linkString.rsplit("href=")
-       linkString2 = linkString1[1].rsplit(">\n")
-       link = linkString2[0].split('"')
+       linkString1 = linkString.rsplit("id=")
+       linkID = linkString1[1].rsplit('">\n')
 
+       #
+       if(linkID[0]!= '2868'):
+            print(linkID[0])
+            urlRAW = "https://ofertadebienes.com/o_detail.asp?id={}"
+            urlItem = urlRAW.format(linkID[0])      
+            htmlItem = urlopen(urlItem).read().decode()
+                            
+            res2 = BeautifulSoup(htmlItem, "html5lib")
+            itemTags = res2.findAll("div", id="detalle_bienes")
+            
+            for taki in itemTags:
+                
+                raw = taki.find("div", class_="descripcion")
+                rawString = str(raw)
+
+                #No de Finca
+                noFin0 = rawString.rsplit('</strong>')
+                noFin1 = noFin0[1].rsplit(': ')
+                
+                #Direccion
+                dir0 = rawString.rsplit('Dirección:</strong> ')
+                dir1 = dir0[1].rsplit(' <br/>')
+                
+                #Area de Construccion
+                ac0 = rawString.rsplit('de:</strong> ')
+                ac1 = ac0[1].rsplit('<sup>')
+                
+                #Tipo
+                tipo0 = rawString.rsplit('<h5><strong>\n\t\t\t\t\t\t\t')
+                tipo1 = tipo0[1].rsplit('\n')
+
+                #Dimencion del Terreno
+                if(tipo1[0] == 'Apartamento'):
+                    dim1[0] = ' '
+                else: 
+                    dim0 = rawString.rsplit('Terreno:</strong> ')
+                    dim1 = dim0[1].rsplit('<sup>')
+                
+                #Detalles
+                deta0 = rawString.rsplit('Detalles:</strong> ')
+                deta1 = deta0[1].rsplit('\n')
+                
+                #Financiamiento Maximo
+                #fm0 = rawString.rsplit('ximo: <strong>')
+                #fm1 = fm0[1].rsplit('</strong>')
+                                        
        #JSON
        data['property'].append({
-           'ID': linkString2[0],
-           'description': description.getText(),
-           'image': imagenURL[1],
-           'price': price.getText(),
-           'link': link[1]
-
+           'ID_link':               linkID[0],
+           'Description':           description.getText(),
+           'Image':                 imagenURL[1],
+           'Price':                 price.getText(),
+           'Typo':                  tipo1[0],
+           #'Financiamiento Maximo': fm1[0],
+           'Details':               deta1[0],
+           'Dimension':             dim1[0],
+           'Area':                  ac1[0],
+           'Address':               dir1[0],
+           'NoFinca':               noFin1[1]
+           
+           
        })
            
-       #print(link)    #si quieres imprimir el precio por price.getText() dentro del print
+       #print(linkID[0])    #si quieres imprimir el precio por price.getText() dentro del print
 
 # print(data['property'])
 with open('data.json', 'w') as json_file:
